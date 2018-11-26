ce811832dab4c7e857ebd86bca0a346fb950a701
unalakunal <unal.akunal@gmail.com>
2018-11-26 01:45:45 +0100

    optimize random forests with grid search

--- titanic.py
+++ titanic.py
@@ -10,6 +10,8 @@ import math
 
 _class = "Survived"
 _features = ["Pclass", "Sex", "Age", "SibSp", "Parch",  "Fare", "Embarked"]
+# declare shared global var.s
+train_x, train_y, val_x, val_y, test_x = [0, 0, 0, 0, 0]
 
 
 def enum_sex(df):
@@ -25,7 +27,7 @@ dev_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
 
 
-def generate_report(clf, x, y, printout=False, to_file=False):
+def generate_report(clf, printout=False, to_file=False):
     """
     Predicts for validation set and calculates 
     precision,recall,f1-score and support
@@ -35,10 +37,11 @@ def generate_report(clf, x, y, printout=False, to_file=False):
     print("creating classification report for ", clf_name)
     if clf_name == "GridSearchCV":
         clf = clf.best_estimator_
+        clf_name = clf_name + "_" + clf.__class__.__name__
 
-    pred_val_y = clf.predict(x)
+    pred_val_y = clf.predict(val_x)
     # calculate metrics for validation set
-    report = classification_report(y_true=y, y_pred=pred_val_y)
+    report = classification_report(y_true=val_y, y_pred=pred_val_y)
     if printout:
         print(report)
     if to_file:
@@ -47,6 +50,8 @@ def generate_report(clf, x, y, printout=False, to_file=False):
 
 
 def preprocess():
+    global train_x, train_y, val_x, val_y, test_x
+
     # enumerate sex & embarked for both df.s
     enum_sex(dev_df)
     enum_embarked(dev_df)
@@ -67,18 +72,17 @@ def preprocess():
     # use Imputer to change NaN val.s with column mean val.s
     imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
     imp = imp.fit(train_x)
-    train_x_imp = imp.fit(train_x).transform(train_x)
-    val_x_imp = imp.fit(val_x).transform(val_x)
-    test_x_imp = imp.fit(test_x).transform(test_x)
-
-    return train_x_imp, train_y, val_x_imp, val_y, test_x_imp
+    train_x = imp.fit(train_x).transform(train_x)
+    val_x = imp.fit(val_x).transform(val_x)
+    test_x = imp.fit(test_x).transform(test_x)
 
 
-def predict_test(clf, test_x, to_file=False):
+def predict_test(clf, to_file=False):
     clf_name = clf.__class__.__name__
     print("creating predictions for", clf_name)
     if clf_name == "GridSearchCV":
         clf = clf.best_estimator_
+        clf_name = clf_name + "_" + clf.__class__.__name__
 
     pred_y = clf.predict(test_x)
     if to_file:
@@ -94,16 +98,17 @@ def write_file(pred_y, fname="res"):
     res.to_csv("pred/" + fname + ".csv")
 
 
-def main():
-    train_x, train_y, val_x, val_y, test_x = preprocess()
-
+def decision_tree():
     # decision tree
     dtree = DecisionTreeClassifier(criterion="gini")
     dtree.fit(train_x, train_y)
-    generate_report(dtree, val_x, val_y, printout=True, to_file=True)
+    generate_report(dtree, printout=True, to_file=True)
+    return dtree
 
+
+def grid_decision_tree():
     # grid search on top of decision tree
-    grid_dtree = GridSearchCV(
+    grid_dt = GridSearchCV(
         DecisionTreeClassifier(criterion="gini"),
         param_grid={
             "min_samples_split": range(2, 10),
@@ -112,20 +117,58 @@ def main():
         },
         n_jobs=8
     )
-    grid_dtree.fit(train_x, train_y)
-    print("grid search best param.s", grid_dtree.best_params_)
-    print("grid search best score", grid_dtree.best_score_)
-    generate_report(grid_dtree, val_x, val_y, printout=True, to_file=True)
+    grid_dt.fit(train_x, train_y)
+    print("grid search best param.s", grid_dt.best_params_)
+    print("grid search best score", grid_dt.best_score_)
+    generate_report(grid_dt, printout=True, to_file=True)
+    return grid_dt
 
-    # random forests
-    rforests = RandomForestClassifier(n_estimators=100)
-    rforests.fit(train_x, train_y)
-    generate_report(rforests, val_x, val_y, printout=True, to_file=True)
 
-    # predict test data
-    pred_y_dtree = predict_test(dtree, test_x, to_file=True)
-    pred_y_dtree = predict_test(grid_dtree, test_x, to_file=True)
-    pred_y_dtree = predict_test(rforests, test_x, to_file=True)
+def random_forest():
+    """
+    Simple random forest classifier
+    """
+    rforest = RandomForestClassifier(n_estimators=100)
+    rforest.fit(train_x, train_y)
+    generate_report(rforest, printout=True, to_file=True)
+    return rforest
+
+
+def grid_random_forest():
+    """
+    Grid search on top of decision tree.
+    Most probably a huge overkill for the task.
+    """
+
+    grid_rf = GridSearchCV(
+        RandomForestClassifier(n_estimators=100),
+        param_grid={
+            "min_samples_split": range(2, 10),
+            "max_depth": range(2, 25),
+            "min_samples_leaf": range(1, 10)
+        },
+        n_jobs=8
+    )
+    grid_rf.fit(train_x, train_y)
+    print("grid search best param.s for random forest", grid_rf.best_params_)
+    print("grid search best score for random forest", grid_rf.best_score_)
+    generate_report(grid_rf, printout=True, to_file=True)
+    return grid_rf
+
+
+def main():
+    preprocess()
+
+    dtree = decision_tree()
+    grid_dt = grid_decision_tree()
+    rforest = random_forest()
+    grid_rf = grid_random_forest()
+
+    # predict test data w/ all of classifiers
+    predict_test(dtree, to_file=True)
+    predict_test(grid_dt, to_file=True)
+    predict_test(rforest, to_file=True)
+    predict_test(grid_rf, to_file=True)
 
 
 if __name__ == "__main__":
